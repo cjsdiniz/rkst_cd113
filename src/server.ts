3e9ba927fc3ede67f3d6c02a9cb71ea67c64c274
cjsdiniz <67067863+cjsdiniz@users.noreply.github.com>
2022-02-04 12:04:00 +0000

    TypeORM config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import "reflect-metadata";
 
 const app = express();
 
+app.get("/",  (req, res)  => {
+    return res.json({
+            message: "Welcome!",
+        })
+    })
 app.listen(3000, () => console.log("Server running on port 3000"));
